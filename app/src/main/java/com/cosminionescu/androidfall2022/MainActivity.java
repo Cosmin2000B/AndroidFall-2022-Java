10d7b43da82cac46f2950e1dd152014175cc8b1e
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-11-26 14:35:43 +0200

    [Curs 1 & 2 - 26.11.2022] Initial commit

---
+++ app/src/main/java/com/cosminionescu/androidfall2022/MainActivity.java
@@ -0,0 +1,50 @@
+package com.cosminionescu.androidfall2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewDisplayAccount;
+
+
+    /**
+     * Legatura dintre layout si activitate
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        // clasa R pt resurse -> ia layout
+        setContentView(R.layout.login);
+
+        editTextEmail = findViewById(R.id.editTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        textViewDisplayAccount = findViewById(R.id.textViewDisplayAccount);
+    }
+
+    public void loginOnClick(View view) {
+
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        textViewDisplayAccount.setText(
+                new StringBuilder()
+                        .append(email)
+                        .append(" ")
+                        .append(password)
+        );
+    }
+}
