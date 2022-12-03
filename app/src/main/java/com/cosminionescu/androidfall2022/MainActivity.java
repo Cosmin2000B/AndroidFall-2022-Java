f8fef9556e91b02295be2e0e351672cfd9e50bcc
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2022-12-03 10:53:50 +0200

    [Curs 3 & 4 - 03.12.2022] Enhanced login onClick
       * enhanced login onClick logic;
       * added second activity + layout;
       * added transition via button from one activity to the other;

--- app/src/main/java/com/cosminionescu/androidfall2022/MainActivity.java
+++ app/src/main/java/com/cosminionescu/androidfall2022/MainActivity.java
@@ -2,19 +2,30 @@ package com.cosminionescu.androidfall2022;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 /**
  *
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private EditText editTextEmail;
     private EditText editTextPassword;
     private TextView textViewDisplayAccount;
+
+    private Button buttonDisplayAndroid;
 
 
     /**
@@ -33,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
         editTextEmail = findViewById(R.id.editTextEmailAddress);
         editTextPassword = findViewById(R.id.editTextPassword);
 
+        buttonDisplayAndroid = findViewById(R.id.buttonAboutAndroid);
+        buttonDisplayAndroid.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent moveFromMainToAboutAndroid =
+                        new Intent(MainActivity.this, AboutAndroid.class);
+                startActivity(moveFromMainToAboutAndroid);
+            }
+        });
+
         textViewDisplayAccount = findViewById(R.id.textViewDisplayAccount);
         textViewDisplayAccount.setOnClickListener(this::loginOnClick);
     }
@@ -42,11 +64,31 @@ public class MainActivity extends AppCompatActivity {
         String email = editTextEmail.getText().toString();
         String password = editTextPassword.getText().toString();
 
-        textViewDisplayAccount.setText(
-                new StringBuilder()
-                        .append(email)
-                        .append(" ")
-                        .append(password)
-        );
+        if (email.length() > 0 && password.length() > 0) {
+
+            if (Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                textViewDisplayAccount.setText(
+                        new StringBuilder()
+                                .append(email)
+                                .append(" ")
+                                .append(password)
+                );
+            } else {
+                editTextEmail.setError("Please enter valid email");
+                Log.e(TAG, "email doesn't have a valid format");
+            }
+        } else if (email.length() <= 0) {
+            editTextEmail.setError("Please add your email address");
+            // Log.e <=> Log.error
+            // Log.e(TAG, "email is empty"); // log valid
+            Toast.makeText(
+                            MainActivity.this,
+                            "Email adress is not valid",
+                            Toast.LENGTH_SHORT
+                    ).show();
+        } else if (password.length() <= 0) {
+            editTextPassword.setError("Please add your password");
+            Log.e(TAG, "password is empty");
+        }
     }
 }
