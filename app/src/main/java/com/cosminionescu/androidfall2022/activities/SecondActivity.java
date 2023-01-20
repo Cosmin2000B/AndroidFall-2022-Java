85823501348e6d15025b9e2631e217c3c283b40d
Cosmin2000B <cosmin.ionescu@s.utm.ro>
2023-01-20 21:37:10 +0200

    [Curs 7 & 8] Added example of dynamic Fragment

--- app/src/main/java/com/cosminionescu/androidfall2022/activities/SecondActivity.java
+++ app/src/main/java/com/cosminionescu/androidfall2022/activities/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.cosminionescu.androidfall2022.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -12,5 +13,20 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        insertBlankActivity();
+    }
+
+
+    private void insertBlankActivity() {
+        // begin transaction
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // replace the contents of the container with the new fragment
+        // change in the wanted frame (the placeholder) the fragment displayed
+        transaction.replace(R.id.frameLayoutPlaceholder,new BlankFragment());
+
+        // commit the transaction
+        transaction.commit();
     }
 }
